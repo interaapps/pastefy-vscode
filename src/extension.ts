a050e54d3f39c75612a8a67449b5c12af2cde83f
Julian Gojani <julian@gojani.xyz>
2021-02-02 20:20:06 +0100

    Added new API Support and Auth-Support (Via API-Key)

--- src/extension.ts
+++ src/extension.ts
@@ -4,11 +4,18 @@ import * as querystring from 'querystring';
 
 function createPaste(content: string){
 	if (content.trim() !== "") {
-		axios.post("https://pastefy.ga/create:paste", {
+		axios.post("https://pastefy.ga/api/v2/paste", {
+			title: "asd",
 			content: content
-		}).then((res)=>{
-			vscode.window.showInformationMessage('Pasted! '+"https://pastefy.ga/"+res.data.id);
-			vscode.env.clipboard.writeText("https://pastefy.ga/"+res.data.id);
+		}, {
+			headers: {
+				"x-auth-key": vscode.workspace.getConfiguration("pastefy").get("pastefyAPIKey")
+			}
+		}).then((res)=>{			
+			vscode.window.showInformationMessage('Pasted! '+"https://pastefy.ga/"+res.data.paste.id);
+			vscode.env.clipboard.writeText("https://pastefy.ga/"+res.data.paste.id);
+		}).catch(()=>{
+			vscode.window.showErrorMessage("Error while pasting!");
 		});	
 	} else  {
 		vscode.window.showInformationMessage("You cannot send empty code.");
@@ -26,7 +33,8 @@ export function activate(context: vscode.ExtensionContext) {
 	let editorContextMenu = vscode.commands.registerCommand('pastefy.pasteFromEditorContextMenu', () => {
 		createPaste(vscode.window.activeTextEditor?.document.getText(vscode.window.activeTextEditor?.selection) || "");
 	});
-
+	
+	
 
 
 	context.subscriptions.push(...[
