b97ec4ec7e5cc4e416d04d826713c34d634f19c8
Julian Gojani <julian@gojani.xyz>
2022-06-30 14:26:56 +0200

    Added browser login

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,20 @@
 import * as vscode from 'vscode';
 import axios from 'axios';
-import * as querystring from 'querystring';
 
-function createPaste(content: string, title: string = ""){
-	if (content.trim() !== "") {
-		axios.post("https://pastefy.ga/api/v2/paste", {
-			title,
-			content
-		}, {
+function createPaste(req: {content: string, title: string}){
+	if (req.content.trim() !== "") {
+		console.log(req);
+		
+		axios.post("https://pastefy.ga/api/v2/paste", req, {
 			headers: {
-				"x-auth-key": vscode.workspace.getConfiguration("pastefy").get("pastefyAPIKey")
+				Authorization: `Bearer ${ vscode.workspace.getConfiguration("pastefy").get("pastefyAPIKey") }`
 			}
 		}).then((res)=>{			
 			vscode.window.showInformationMessage('Pasted! '+"https://pastefy.ga/"+res.data.paste.id);
 			vscode.env.clipboard.writeText("https://pastefy.ga/"+res.data.paste.id);
-		}).catch(()=>{
+		}).catch(e => {
+			console.log(e);
+			
 			vscode.window.showErrorMessage("Error while pasting!");
 		});	
 	} else  {
@@ -22,24 +22,69 @@ function createPaste(content: string, title: string = ""){
 	}
 }
 
+function getSimpleFileName(name : string) : string {
+	const splittedFileName = name.split("/")
+	if (splittedFileName.length == 1)
+		return name;
+	return splittedFileName[splittedFileName.length-1]
+}
+
 export function activate(context: vscode.ExtensionContext) {
-	let explorereContextMenu = vscode.commands.registerCommand('pastefy.pasteFromExplorerContextMenu', (clickedFile: vscode.Uri, selectedFiles: vscode.Uri[]) => {
+	const explorereContextMenu = vscode.commands.registerCommand('pastefy.pasteFromExplorerContextMenu', (clickedFile: vscode.Uri, selectedFiles: vscode.Uri[]) => {
 		vscode.workspace.openTextDocument(clickedFile).then((document) => {
-			let splittedFileName = document.fileName.split("/")
-			createPaste(document.getText(), splittedFileName[splittedFileName.length-1]);
+			createPaste({
+				content: document.getText(), 
+				title: getSimpleFileName(document.fileName)
+			});
 		});
 	});
 
-	let editorContextMenu = vscode.commands.registerCommand('pastefy.pasteFromEditorContextMenu', () => {
-		createPaste(vscode.window.activeTextEditor?.document.getText(vscode.window.activeTextEditor?.selection) || "");
+	const editorContextMenu = vscode.commands.registerCommand('pastefy.pasteFromEditorContextMenu', () => {
+		createPaste({
+			content: vscode.window.activeTextEditor?.document.getText(vscode.window.activeTextEditor?.selection) || "", 
+			title: `Snippet from ${vscode.window.activeTextEditor?.document.fileName}`
+		});
 	});
-	
-	
 
 
+	const login = vscode.commands.registerCommand('pastefy.login', async () => {
+		const createAccessRequest = await axios.get("https://pastefy-login.interaapps.de/create").then(r => r.data)
+
+		vscode.env.openExternal(vscode.Uri.parse(createAccessRequest.url))
+
+		let i = 0
+		const id = setInterval(async () => {
+			const res = await axios.get(`https://pastefy-login.interaapps.de/check?key=${ createAccessRequest.key }`).then(r => r.data)
+
+			if (res.success) {
+				vscode.workspace.getConfiguration("pastefy").update('pastefyAPIKey', res.code)
+
+				axios.get(`https://pastefy.ga/api/v2/user`, {
+					headers: {
+						Authorization: `Bearer ${ res.code }`
+					}
+				})
+					.then(r => r.data)
+					.then(user => {
+						console.log(user);
+						
+						vscode.window.showInformationMessage(`Pastefy: Welcome back, ${ user.name }!`);
+					})
+				
+				clearInterval(id)
+			}
+
+			if (i++ > 50) {
+				clearInterval(id)
+			}
+		}, 2500)
+	})
+	
+	
 	context.subscriptions.push(...[
 		explorereContextMenu,
-		editorContextMenu
+		editorContextMenu,
+		login
 	]);
 }
 
